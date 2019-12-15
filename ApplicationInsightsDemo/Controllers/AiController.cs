af0c03ce6100621533cd9a81924a2842df129f3f
Brian Langhoor <brian.langhoor@infocaster.net>
2019-12-15 23:07:32 +0100

    Add project files.

---
+++ ApplicationInsightsDemo/Controllers/AiController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationInsightsDemo.Controllers
+{
+    [ApiController]
+    public class AiController : ControllerBase
+    {
+        // GET api/ai
+        [HttpGet]
+        [Route("api/succes")]
+        public ActionResult<string> Succes()
+        {
+            return Ok("This request was succesfull");
+        }
+
+        [HttpGet]
+        [Route("api/bad")]
+        public ActionResult<string> Bad()
+        {
+            return BadRequest("This request was a bad request");
+        }
+
+        [HttpGet]
+        [Route("api/unauthorized")]
+        public ActionResult<string> UnAuthorized()
+        {
+            return Unauthorized("This request was unauthorized");
+        }
+
+        [HttpGet]
+        [Route("api/exception")]
+        public ActionResult<string> Exception()
+        {
+            throw new NotImplementedException("This method was not implemented");
+        }
+    }
+}
